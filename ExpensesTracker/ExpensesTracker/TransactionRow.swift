ff0f9ecb52964fcb349f5e865a5af47b83293931
kristapspriede <kristaps.priede@gmail.com>
2022-10-26 18:26:24 +0300

    Add TransactionRow view

---
+++ ExpensesTracker/ExpensesTracker/TransactionRow.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionRow.swift
+//  ExpensesTracker
+//
+//  Created by Kristaps Priede on 26/10/2022.
+//
+
+import SwiftUI
+
+struct TransactionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionRow()
+    }
+}
