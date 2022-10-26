95fd50ffea9ae4bc438577fc3cffc6cb34a4eaa9
kristapspriede <kristaps.priede@gmail.com>
2022-10-26 18:10:05 +0300

    Create project

---
+++ ExpensesTracker/ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by Kristaps Priede on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
