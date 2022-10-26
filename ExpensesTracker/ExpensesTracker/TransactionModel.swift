a244551e5465fc358029db579dad22e6e6090eb6
kristapspriede <kristaps.priede@gmail.com>
2022-10-26 18:18:37 +0300

    Update TransactionModel

--- ExpensesTracker/ExpensesTracker/TransactionModel.swift
+++ ExpensesTracker/ExpensesTracker/TransactionModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct Transaction: Identifiable {
+    let id: Int
+    let date: String
+    let institution: String
+    let account: String
+    var merchant: String
+    let amount: Double
+    let type: TransactionType.RawValue
+    var categoryId: Int
+    var category: String
+    var isPending: Bool
+    var isTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool
+    
+    var dateParsed: Date {
+        date.dateParsed()
+    }
+    
+    var signedAmount: Double {
+        return type == TransactionType.credit.rawValue ? amount : -amount
+    }
+}
+enum TransactionType: String {
+    case debit = "debit"
+    case credit = "credit"
+}
